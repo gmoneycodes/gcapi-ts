c8a31a54727e8632aeb3e93dab3bc05b2ea06ed2
gmoneycodes <greg@ruyoga.io>
2024-03-31 12:08:19 +0200

    edit handling

--- exps/index.js
+++ exps/index.js
@@ -1,37 +1,57 @@
-const qrcode = require('qrcode-terminal')
+const qrcode = require('qrcode-terminal');
 const Gcapi = require("../dist/source/gcapi").default;
+const readline = require('readline');
+
+// Create readline interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 const main = async () => {
-    const gtc= new Gcapi({
-        cookiePath: './cookie.json',
-        showQr: false,
-        puppeteer: {
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            ignoreDefaultArgs: ['--disable-extensions'],
-        },
-    })
+    // Prompt user for country code
+    rl.question('Enter country code: ', (countryCode) => {
+        const fullCountryCode = countryCode
+        // Prompt user for phone number
+        rl.question('Enter phone number: ', async (phoneNumber) => {
+            const fullPhoneNumber = phoneNumber;
 
-    gtc.on('qrcode', (value) => {
-        qrcode.generate(value, { small: true })
-    })
+            const gtc = new Gcapi({
+                cookiePath: './cookie.json',
+                showQr: false,
+                puppeteer: {
+                    headless: true,
+                    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+                    ignoreDefaultArgs: ['--disable-extensions'],
+                },
+            });
 
-    gtc.on('logged', (logged) => {
-        if (logged) {
-            console.log('logged')
-        } else {
-            console.log('scan qr code first')
-        }
-    })
+            gtc.on('qrcode', (value) => {
+                qrcode.generate(value, { small: true });
+            });
 
-    gtc.on('error', (error) => {
-        console.error(error)
-    })
+            gtc.on('logged', (logged) => {
+                if (logged) {
+                    console.log('logged');
+                } else {
+                    console.log('scan qr code first');
+                }
+            });
 
-    await gtc.init()
+            gtc.on('error', (error) => {
+                console.error(error);
+            });
 
-    const tags = await gtc.find('ID', '628158043821') // just random phone number
-    console.log(tags)
-}
+            await gtc.init();
 
-main()
+            // Use the full phone number from the user input
+            const tags = await gtc.find(fullCountryCode, fullPhoneNumber);
+            console.log(tags);
+
+            // Close the readline interface
+            rl.close();
+        });
+    });
+};
+
+main();
